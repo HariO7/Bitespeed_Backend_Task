8095a516317fd605408abfd97ebbf373e70b2e15
HariO7 <hariodukkathil@gmail.com>
2023-06-09 14:15:08 +0530

    [main]: added .env file

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = Express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 
 app.get('/',(req:Express.Request,res: Express.Response)=>{
     res.send('Express + Typescript server')
